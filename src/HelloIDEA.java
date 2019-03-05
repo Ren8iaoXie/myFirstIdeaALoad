3599d8e601e5069ca9f05cc63356e151917dec18
Ren8iaoXie <lucky19980921@qq.com>
2019-03-05 14:10:53 +0800

    Initial commit

---
+++ src/HelloIDEA.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloIDEA {
+    public static void main(String[] args) {
+        System.out.println("Hello My IDEA");
+        List<String> list=new ArrayList<String>();
+        list.add(0,"wuxiaotong");
+        list.add(1,"xierenbiao");
+        System.out.println(list);
+
+
+    }
+}
